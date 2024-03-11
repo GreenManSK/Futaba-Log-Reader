ef65e161d59bc13a18cab86d99d2e6e7e8b894d1
Lukáš Kurčík <lukas.kurcik@gmail.com>
2024-03-11 16:51:38 +0100

    Debounce filtering

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -3,33 +3,44 @@ import { useLogsDataContext } from "../contexts/LogsDataContext";
 import { FileUploader } from "./FileUploader";
 import { LogTable } from "./LogTable";
 import { LineLimit } from "./LineLimit";
-import { LogLevel } from "../data/LogParser";
+import { ILogEntry, LogLevel } from "../data/LogParser";
 import { LogLevelFilter } from "./LogLevelFilter";
 import { LogClassFilter } from "./LogClassFilter";
 import { Search } from "./Search";
+import _ from "lodash";
 
 const DEFAULT_LINES_LIMIT = 5000;
 const DEFAULT_LEVEL_FILTER = [LogLevel.WARNING, LogLevel.ERROR, LogLevel.UNKNOWN];
+const FILTER_DEBOUNCE = 250;
 
 export const Content = () => {
     const { data } = useLogsDataContext();
+    const [filteredData, setFilteredData] = React.useState([] as ILogEntry[]);
 
     const [lineLimit, setLineLimit] = React.useState(DEFAULT_LINES_LIMIT);
     const [levelFilter, setLevelFilter] = React.useState(new Set(DEFAULT_LEVEL_FILTER));
     const [excludedClasses, setExcludedClasses] = React.useState(new Set<string>());
     const [search, setSearch] = React.useState("");
 
-    const filteredData = React.useMemo(() => {
-        if (!data) {
-            return [];
+    const updateFilteredData = React.useMemo(() => {
+        const update = (data: ILogEntry[] | undefined, lineLimit: number, levelFilter: Set<LogLevel>, excludedClasses: Set<string>, search: string) => {
+            if (!data) {
+                return [];
+            }
+            const prefiltered = data
+                .filter(entry => levelFilter.has(entry.level) && !excludedClasses.has(entry.loggingClass));
+            if (!search) {
+                setFilteredData(prefiltered.slice(0, lineLimit));
+            }
+            const lowerCaseSearch = search.toLowerCase();
+            setFilteredData(prefiltered.filter(entry => entry.matchesFilter(lowerCaseSearch)).slice(0, lineLimit));
         }
-        const prefiltered = data
-            .filter(entry => levelFilter.has(entry.level) && !excludedClasses.has(entry.loggingClass));
-        if (!search) {
-            return prefiltered.slice(0, lineLimit);
-        }
-        return prefiltered.filter(entry => entry.matchesFilter(search)).slice(0, lineLimit);
-    }, [data, lineLimit, levelFilter, excludedClasses, search]);
+        return _.debounce(update, FILTER_DEBOUNCE);
+    }, [setFilteredData]);
+
+    React.useEffect(() => {
+        updateFilteredData(data, lineLimit, levelFilter, excludedClasses, search);
+    }, [data, lineLimit, levelFilter, excludedClasses, search, updateFilteredData]);
 
     if (!data) {
         return <FileUploader />;
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -5,15 +5,8 @@ interface ISearchProps {
     setSearch: (search: string) => void;
 }
 
-const UPDATE_DELAY = 250;
 export const Search = (props: ISearchProps) => {
     const { search, setSearch } = props;
-    const [internalSearch, setInternalSearch] = React.useState(search);
 
-    React.useEffect(() => {
-        const timeout = setTimeout(() => setSearch(internalSearch.toLowerCase()), UPDATE_DELAY);
-        return () => clearTimeout(timeout);
-    }, [internalSearch, setSearch]);
-
-    return <input type="text" value={internalSearch} onChange={e => setInternalSearch(e.target.value)} placeholder="Filter by" />
+    return <input type="text" value={search} onChange={e => setSearch(e.target.value)} placeholder="Filter by" />
 }
