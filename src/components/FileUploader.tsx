debae334190ff4c73188b969c12c3d8cc7455aa0
GreenManSK <lukas.kurcik@gmail.com>
2024-03-14 14:00:51 +0100

    add nice images

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -1,31 +1,35 @@
 import React from "react";
 import { useLogsDataContext } from "../contexts/LogsDataContext";
-import './FileUploader.css';
+import "./FileUploader.css";
 
 export const FileUploader = () => {
-    const { readData } = useLogsDataContext();
+  const { readData } = useLogsDataContext();
 
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files?.[0];
-        if (file) {
-            const fileReader = new FileReader();
-            fileReader.onload = (e) => {
-                const result = e.target?.result;
-                if (!result) return;
-                if (typeof result === 'string') {
-                    readData(file.name, result);
-                } else {
-                    readData(file.name, (new TextDecoder()).decode(result));
-                }
-            };
-            fileReader.readAsText(file);
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const fileReader = new FileReader();
+      fileReader.onload = (e) => {
+        const result = e.target?.result;
+        if (!result) return;
+        if (typeof result === "string") {
+          readData(file.name, result);
+        } else {
+          readData(file.name, new TextDecoder().decode(result));
         }
+      };
+      fileReader.readAsText(file);
     }
+  };
 
-    return (
-        <div className="file-uploader">
-            <h1>Upload or drag-n-drop your logs</h1>
-            <input type="file" onChange={handleFileChange} />
-        </div>
-    );
+  return (
+    <div className="file-uploader">
+      <div className="futaba"></div>
+      <h1>Upload or drag-n-drop your logs</h1>
+      <div>
+        <input type="file" onChange={handleFileChange} />
+      </div>
+      <div></div>
+    </div>
+  );
 };
--- src/components/LogReader/LogTable.tsx
+++ src/components/LogReader/LogTable.tsx
@@ -1,53 +1,68 @@
 import React from "react";
-import './LogTable.css';
+import "./LogTable.css";
 import { ILogEntry, LogLevel } from "../../data/ILogEntry";
+import { Heart, HeartOff } from "lucide-react";
 
 interface ILogTableProps {
-    data: ILogEntry[],
-    favourites: Set<number>;
-    setFavourites: (favourites: Set<number>) => void;
+  data: ILogEntry[];
+  favourites: Set<number>;
+  setFavourites: (favourites: Set<number>) => void;
 }
 
 export const LogTable = (props: ILogTableProps) => {
-    const { data, favourites, setFavourites } = props;
-    const toggleFavourite = (id: number) => {
-        const newFavourites = new Set(favourites);
-        if (newFavourites.has(id)) {
-            newFavourites.delete(id);
-        } else {
-            newFavourites.add(id);
-        }
-        setFavourites(newFavourites);
+  const { data, favourites, setFavourites } = props;
+  const toggleFavourite = (id: number) => {
+    const newFavourites = new Set(favourites);
+    if (newFavourites.has(id)) {
+      newFavourites.delete(id);
+    } else {
+      newFavourites.add(id);
     }
+    setFavourites(newFavourites);
+  };
 
-    return (
-        <table className="logs">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Date</th>
-                    <th>Level</th>
-                    <th>Class</th>
-                    <th>Message</th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((entry, index) => (
-                    <tr key={index} className={`level-${entry.level}`}>
-                        <td className="number">
-                            <button className="fav-button" onClick={() => toggleFavourite(entry.id)} title={favourites.has(entry.id) ? "Add to favourites" : "Remove from favourites"}>{favourites.has(entry.id) ? "🫀" : "♡"}</button>
-                            {entry.id}
-                        </td>
-                        <td className="date">{entry.dateText}</td>
-                        <td>{LogLevel[entry.level]}</td>
-                        <td className="class-name">
-                            {entry.loggingClass}
-                            {entry.callId && <div className="call-id">{entry.callId}</div>}
-                        </td>
-                        <td className="message">{entry.message}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    );
-}
+  return (
+    <table className="logs">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Date</th>
+          <th>Level</th>
+          <th>Class</th>
+          <th>Message</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((entry, index) => (
+          <tr key={index} className={`level-${entry.level}`}>
+            <td className="number">
+              <button
+                className="fav-button"
+                onClick={() => toggleFavourite(entry.id)}
+                title={
+                  favourites.has(entry.id)
+                    ? "Add to favourites"
+                    : "Remove from favourites"
+                }
+              >
+                {favourites.has(entry.id) ? (
+                  <HeartOff fill="white" size={12} />
+                ) : (
+                  <Heart fill="white" size={12} />
+                )}
+              </button>
+              {entry.id}
+            </td>
+            <td className="date">{entry.dateText}</td>
+            <td>{LogLevel[entry.level]}</td>
+            <td className="class-name">
+              {entry.loggingClass}
+              {entry.callId && <div className="call-id">{entry.callId}</div>}
+            </td>
+            <td className="message">{entry.message}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
